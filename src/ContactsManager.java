0a869211fb6abc0f479baf46552eaa903f3e4469
MjGrace-ops <mjzid81@gmail.com>
2026-05-19 14:50:46 +0000

    last commit

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -10,14 +10,23 @@ public class ContactsManager {
                 this.myFriends = new Contact[500];
         }
 
-        // Ajouter un contact
+        /**
+         * Ajoute un contact dans le tableau
+         * @param contact le contact a ajouter dans le tableau
+         */
         void addContact(Contact contact) {
 
                 myFriends[friendsCount] = contact;
                 friendsCount++;
         }
 
-        // Rechercher un contact
+
+
+        /**
+         * Recherche un contact par son nom
+         * @param searchName le com recherché
+         * @return le contact correspondant sinon renvoie null
+         */
         Contact searchContact(String searchName) {
 
                 for (int i = 0; i < friendsCount; i++) {
